0a23973329f7c3527503efa6eb91aa11fc7724c6
IsaacYu <yuisaac@outlook.com>
2026-01-10 22:13:42 -0500

    set up three fibre

--- app/dashboard/layout/page.tsx
+++ app/dashboard/layout/page.tsx
@@ -1,94 +1,96 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { tryParseInt } from "@/app/services/parse";
-import useComponents from "@/app/hooks/useComponents";
-import { ComponentDetails } from "@/types";
-import ComponentTile from "./componentTile";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, TransformControls } from "@react-three/drei";
+import { useState } from "react";
 
 export default function Page() {
-  const [gridX, setGridX] = useState<number>(0);
-  const [gridY, setGridY] = useState<number>(0);
-  const [componentsGrid, setComponentsGrid] = useState<ComponentDetails[][]>();
-  const { components, fetchComponents } = useComponents();
-
-  useEffect(() => {
-    fetchComponents();
-  }, []);
-
-  useEffect(() => {
-    if (!components) return;
-
-    setGridX(Math.max(...components.map((comp) => comp.x)) + 1);
-    setGridY(Math.max(...components.map((comp) => comp.y)) + 1);
-  }, [components]);
-
-  useEffect(() => {
-    if (!components || gridX == 0 || gridY == 0) return;
-
-    const tempComponentsGrid: ComponentDetails[][] = new Array(gridX)
-      .fill(null)
-      .map(() => new Array(gridY).fill(null));
-
-    components.forEach((comp) => {
-      tempComponentsGrid[comp.x][comp.y] = { ...comp };
-    });
-
-    setComponentsGrid(tempComponentsGrid);
-  }, [gridX, gridY, components]);
+  const [orbitEnabled, setOrbitEnabled] = useState(true);
 
   return (
-    <div className="h-full w-full flex flex-col items-center">
-      <form className="flex flex-row gap-5">
-        <div className="mb-5">
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            Grid Size X
-          </label>
-          <input
-            value={gridX}
-            onChange={(e) => setGridX(tryParseInt(e.target.value) ?? 0)}
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base block w-full px-3 py-2.5"
-            required
-          />
-        </div>
-
-        <div className="mb-5">
-          <label className="block mb-2.5 text-sm font-medium text-heading">
-            Grid Size Y
-          </label>
-          <input
-            value={gridY}
-            onChange={(e) => setGridY(tryParseInt(e.target.value) ?? 0)}
-            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base block w-full px-3 py-2.5"
-            required
-          />
-        </div>
-      </form>
-
-      <div
-        className="grid gap-2"
-        style={{
-          gridTemplateColumns: `repeat(${gridY}, minmax(0, 1fr))`,
-          width: "fit-content",
+    <div className="h-full w-full">
+      <Canvas
+        camera={{
+          near: 0.1,
+          far: 1000,
+          zoom: 1,
+          position: [0, 0, 5],
         }}
       >
-        {componentsGrid?.map((rowDetails, i) =>
-          rowDetails.map((details, j) => {
-            const isNull = !details;
-            return (
-              <ComponentTile
-                key={`${i}-${j}`}
-                id={isNull ? 0 : details.id}
-                type={isNull ? "N/A" : details.type}
-                pin={isNull ? 0 : details.pin}
-                x={i}
-                y={j}
-                onRefresh={fetchComponents}
-              />
-            );
-          })
-        )}
-      </div>
+        <TransformControls
+          mode="translate"
+          onMouseDown={() => setOrbitEnabled(false)}
+          onMouseUp={() => setOrbitEnabled(true)}
+        >
+          <>
+            <mesh>
+              <boxGeometry />
+              <meshStandardMaterial color="orange" />
+            </mesh>
+          </>
+        </TransformControls>
+        <mesh>
+          <planeGeometry args={[10, 10, 10]} />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+        <ambientLight args={[0xffffff]} intensity={0.2} />
+        <directionalLight position={[1, 1, 1]} intensity={0.8} />
+
+        {orbitEnabled && <OrbitControls />}
+      </Canvas>
     </div>
   );
 }
+
+// <div className="h-full w-full flex flex-col items-center">
+//   <form className="flex flex-row gap-5">
+//     <div className="mb-5">
+//       <label className="block mb-2.5 text-sm font-medium text-heading">
+//         Grid Size X
+//       </label>
+//       <input
+//         value={gridX}
+//         onChange={(e) => setGridX(tryParseInt(e.target.value) ?? 0)}
+//         className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base block w-full px-3 py-2.5"
+//         required
+//       />
+//     </div>
+
+//     <div className="mb-5">
+//       <label className="block mb-2.5 text-sm font-medium text-heading">
+//         Grid Size Y
+//       </label>
+//       <input
+//         value={gridY}
+//         onChange={(e) => setGridY(tryParseInt(e.target.value) ?? 0)}
+//         className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base block w-full px-3 py-2.5"
+//         required
+//       />
+//     </div>
+//   </form>
+
+//   <div
+//     className="grid gap-2"
+//     style={{
+//       gridTemplateColumns: `repeat(${gridY}, minmax(0, 1fr))`,
+//       width: "fit-content",
+//     }}
+//   >
+//     {componentsGrid?.map((rowDetails, i) =>
+//       rowDetails.map((details, j) => {
+//         const isNull = !details;
+//         return (
+//           <ComponentTile
+//             key={`${i}-${j}`}
+//             id={isNull ? 0 : details.id}
+//             type={isNull ? "N/A" : details.type}
+//             pin={isNull ? 0 : details.pin}
+//             x={i}
+//             y={j}
+//             onRefresh={fetchComponents}
+//           />
+//         );
+//       })
+//     )}
+//   </div>
+// </div>
